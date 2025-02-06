236e0d9b80048e90ef95449a436ea70df612f8ac
Đào Nhật Trung <79350606+caubuonviaiV@users.noreply.github.com>
2025-02-06 16:05:38 +0700

    footer new

--- resources/views/user/layout/footer.blade.php
+++ resources/views/user/layout/footer.blade.php
@@ -1,116 +1,172 @@
-<footer class="dk-footer">
-    <div class="container">
-        <div class="fixed-bg bg3"></div>
-        <div class="row top-footer">
-            <div class="phone-div">
-                <div class="border-circle">
-                    <div class="phone-circle">
-                        <a href="#top" class="ext-link">
-                            <img class="gotop" src="{{ asset('teamplate/img/go.png') }}">
-                        </a>
-                    </div>
-                </div>
+<footer>
+    <div class="gotop">
+        <div class="border-circle">
+            <div class="phone-circle">
+                <a href="#top" class="ext-link">
+                    <img class="gotop_icon" src="{{ asset('teamplate/img/gotop.png') }}">
+                </a>
             </div>
+        </div>
+    </div>
+    <div class="mid-footer">
+        <div class="container">
             <div class="row">
-                <div class="col-md-12 col-lg-4">
-                    <div class="dk-footer-box-info">
-                        <img src="{{ asset('teamplate/img/logobuy-01.png') }}" alt="footer_logo"
-                            style="display: inline-block;">
-                        <h3>Mì Quảng Bà Mua</h3>
-                        <p class="footer-info-text">
-                            Mỳ quảng trở thành món ăn đặc trưng của cả miền Trung, trở thành món ngon không thể bỏ lỡ
-                            nếu một lần nếm thử nếu bạn đến Đà Nẵng.
-                        </p>
-                        <p style="float: left; font-weight: 400">Liên hệ tôi:</p>
-                        <div class="follow_us">
-                            <ul>
-                                <li>
-                                    <a href="https://www.facebook.com/dntrung044/"><img
-                                            src="{{ asset('teamplate/img/facebook_icon.svg') }}"
-                                            data-src="{{ asset('teamplate/img/facebook_icon.svg') }}" alt=""
-                                            class="lazy loaded" data-was-processed="true">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://github.com/dntrung044"><img
-                                            src="{{ asset('teamplate/img/github-square.svg') }}"
-                                            data-src="{{ asset('teamplate/img/github-square.svg') }}" alt=""
-                                            class="lazy loaded" data-was-processed="true">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.youtube.com/@dntrung044"><img
-                                            src="{{ asset('teamplate/img/youtube_icon.svg') }}"
-                                            data-src="{{ asset('teamplate/img/youtube_icon.svg') }}" alt=""
-                                            class="lazy loaded" data-was-processed="true">
-                                    </a>
-                                </li>
+                <div class="col-12 col-lg-4 ft-info">
+                    <a href="/" class="logo_foo" title="Logo">
+                        <img width="220" src="{{ asset('teamplate/img/logo1.png') }}" alt="footer_logo">
+                    </a>
+                    <div class="des_foo">
+                        Ẩm thực Xứ Quảng - Hương vị truyền thống, đậm đà xứ Quảng.
+                    </div>
+
+                    <div class="list-menu toggle-mn">
+                        <span>Cửa hàng chính</span>
+                        </h4>
+                        <div class="content-contact clearfix">
+                            <span class="list_footer">
+                                <b>Địa chỉ: </b>
+                                460 Trần Đại Nghĩa, Ngũ Hành Sơn, TP Đà Nẵng
+                            </span>
+                        </div>
+                        <div class="content-contact clearfix">
+                            <span class="list_footer">
+                                <b>Điện thoại: </b>
+                                <a title="1900 0000" href="tel:19000000">
+                                    1900 0000
+                                </a>
+                            </span>
+                        </div>
+                        <div class="content-contact clearfix">
+                            <span class="list_footer">
+                                <b>Email: </b>
+                                <a title="trungdao10a1@gmail.com" href="mailto:trungdao10a1@gmail.com">
+                                    trungdao10a1@gmail.com
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                    <a class="see-store" href="/he-thong-nha-hang" title="Hệ thống cửa hàng">Hệ thống nhà hàng</a>
+                </div>
+                <div class="col-12 col-lg-5 ft-menu">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 link-list col-footer footer-click">
+                            <h4 class="title-menu title-menu2">
+                                Chính sách
+                            </h4>
+                            <ul class="list-menu hidden-mobile">
+                                <li><a href="/chinh-sach-thanh-vien" title="Chính sách thành viên">Chính sách thành
+                                        viên</a></li>
+                                <li><a href="/chinh-sach-thanh-toan" title="Chính sách thanh toán">Chính sách thanh
+                                        toán</a></li>
+                                <li><a href="/chinh-sach-bao-mat" title="Chính sách bảo mật">Chính sách bảo mật</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-sm-6 link-list col-footer footer-click">
+                            <h4 class="title-menu title-menu2">
+                                Hướng dẫn
+                            </h4>
+                            <ul class="list-menu hidden-mobile">
+                                <li><a href="/huong-dan-mua-hang" title="Hướng dẫn mua hàng">Hướng dẫn mua hàng</a></li>
+                                <li><a href="/huong-dan-thanh-toan" title="Hướng dẫn thanh toán">Hướng dẫn thanh
+                                        toán</a></li>
+                                <li><a href="/lien-he" title="Liên hệ">Liên hệ</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-md-12 col-lg-8">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-6">
-                            <div class="footer-widget footer-left-widget">
-                                <div class="section-heading">
-                                    <h3>Hệ thống Mỳ Quảng Bà Mua:</h3>
-                                    <span class="animate-border border-black"></span>
-                                </div>
-                                <ul>
-                                    <li>
-                                        <a href="#">95A Nguyễn Tri Phương, TP. Đà Nẵng</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">40 Ngũ Hành Sơn, TP. Đà Nẵng</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">235 Núi Thành, Q. Hải Châu, TP. Đà Nẵng</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- End Footer Widget -->
-                        </div>
-                        <!-- End col -->
-                        <div class="col-md-12 col-lg-6">
-                            <div class="footer-widget footer-left-widget">
-                                <div class="section-heading">
-                                    <h3>Tùy Chọn Thanh Toán</h3>
-                                    <span class="animate-border border-black"></span>
-                                </div>
-                                <div class="col mb-4 gallery">
-                                    <img alt="err" class="thanhtoan" style="width: 250px; height: 120px;"
-                                        src="{{ asset('teamplate/img/thanhtoan.png') }}">
-                                </div>
-
-
-                                <div class="section-heading">
-                                    <h3>Phương thức vận chuyển</h3>
-                                    <span class="animate-border border-black"></span>
-                                </div>
-                                <div class="col gallery">
-                                    <img alt="err" style="width: 250px; height: 120px;"
-                                        src="{{ asset('teamplate/img/shippers.png') }}">
-                                </div>
-                            </div>
+                <div class="col-12 col-lg-3 link-list col-footer footer-click">
+                    <h4 class="title-menu title-menu2">
+                        Kết nối với chúng tôi
+                    </h4>
+                    <div class="social-footer list-menu hidden-mobile">
+                        <div class="social toggle-mn">
+                            <a class="fb" href="https://www.facebook.com/sapowebvietnam/" target="_blank"
+                                aria-label="Facebook" title="Theo dõi trên Facebook">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    width="25px" height="25px" viewBox="0 0 96.124 96.123"
+                                    style="enable-background:new 0 0 96.124 96.123;" xml:space="preserve">
+                                    <path
+                                        d="M72.089,0.02L59.624,0C45.62,0,36.57,9.285,36.57,23.656v10.907H24.037c-1.083,0-1.96,0.878-1.96,1.961v15.803   c0,1.083,0.878,1.96,1.96,1.96h12.533v39.876c0,1.083,0.877,1.96,1.96,1.96h16.352c1.083,0,1.96-0.878,1.96-1.96V54.287h14.654   c1.083,0,1.96-0.877,1.96-1.96l0.006-15.803c0-0.52-0.207-1.018-0.574-1.386c-0.367-0.368-0.867-0.575-1.387-0.575H56.842v-9.246   c0-4.444,1.059-6.7,6.848-6.7l8.397-0.003c1.082,0,1.959-0.878,1.959-1.96V1.98C74.046,0.899,73.17,0.022,72.089,0.02z"
+                                        data-original="#000000" class="active-path" data-old_color="#000000"
+                                        fill="#EBE7E7"></path>
+                                </svg>
+                            </a>
+                            <a class="zl" href="https://twitter.com" target="_blank" aria-label="Zalo"
+                                title="Theo dõi trên Zalo">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 161.5 161.5">
+                                    <title>Zalo logo</title>
+                                    <path
+                                        d="M504.54,431.79h14.31c19.66,0,31.15,2.89,41.35,8.36a56.65,56.65,0,0,1,23.65,23.65c5.47,10.2,8.36,21.69,8.36,41.35V519.4c0,19.66-2.89,31.15-8.36,41.35a56.65,56.65,0,0,1-23.65,23.65c-10.2,5.47-21.69,8.36-41.35,8.36H504.6c-19.66,0-31.15-2.89-41.35-8.36a56.65,56.65,0,0,1-23.65-23.65c-5.47-10.2-8.36-21.69-8.36-41.35V505.14c0-19.66,2.89-31.15,8.36-41.35a56.65,56.65,0,0,1,23.65-23.65C473.39,434.68,484.94,431.79,504.54,431.79Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                    <path
+                                        d="M592.21,517v2.35c0,19.66-2.89,31.15-8.35,41.35a56.65,56.65,0,0,1-23.65,23.65c-10.2,5.47-21.69,8.36-41.35,8.36H504.6c-16.09,0-26.7-1.93-35.62-5.63L454.29,572Z"
+                                        transform="translate(-431.25 -431.25)"
+                                        style="fill:#001a33;opacity:0.11999999731779099;isolation:isolate" />
+                                    <path
+                                        d="M455.92,572.51c7.53.83,16.94-1.31,23.62-4.56,29,16,74.38,15.27,101.84-2.3q1.6-2.4,3-5c5.49-10.24,8.39-21.77,8.39-41.5v-14.3c0-19.73-2.9-31.26-8.39-41.5a56.86,56.86,0,0,0-23.74-23.74c-10.24-5.49-21.77-8.39-41.5-8.39H504.76c-16.8,0-27.71,2.12-36.88,6.15q-.75.67-1.47,1.37c-26.89,25.92-28.93,82.11-6.13,112.64l.08.14c3.51,5.18.12,14.24-5.18,19.55C454.32,571.89,454.63,572.39,455.92,572.51Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#fff" />
+                                    <path
+                                        d="M497.35,486.34H465.84v6.76h21.87l-21.56,26.72a6.06,6.06,0,0,0-1.17,4v1.72h29.73a2.73,2.73,0,0,0,2.7-2.7v-3.62h-23l20.27-25.43,1.11-1.35.12-.18a8,8,0,0,0,1.41-5Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                    <path d="M537.47,525.54H542v-39.2h-6.76v36.92A2.27,2.27,0,0,0,537.47,525.54Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                    <path
+                                        d="M514.37,495.07a15.36,15.36,0,1,0,15.36,15.36A15.36,15.36,0,0,0,514.37,495.07Zm0,24.39a9,9,0,1,1,9-9A9,9,0,0,1,514.37,519.46Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                    <path
+                                        d="M561.92,494.82A15.48,15.48,0,1,0,577.4,510.3,15.5,15.5,0,0,0,561.92,494.82Zm0,24.64a9.09,9.09,0,1,1,9.09-9.09A9.07,9.07,0,0,1,561.92,519.46Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                    <path d="M526.17,525.54h3.62V495.93h-6.33v27A2.72,2.72,0,0,0,526.17,525.54Z"
+                                        transform="translate(-431.25 -431.25)" style="fill:#0068ff" />
+                                </svg>
+                            </a>
+                            <a class="yt" href="https://www.youtube.com/" target="_blank" aria-label="Youtube"
+                                title="Theo dõi trên Youtube">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"
+                                    style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                                    <path
+                                        d="M490.24,113.92c-13.888-24.704-28.96-29.248-59.648-30.976C399.936,80.864,322.848,80,256.064,80c-66.912,0-144.032,0.864-174.656,2.912c-30.624,1.76-45.728,6.272-59.744,31.008C7.36,138.592,0,181.088,0,255.904C0,255.968,0,256,0,256c0,0.064,0,0.096,0,0.096v0.064c0,74.496,7.36,117.312,21.664,141.728c14.016,24.704,29.088,29.184,59.712,31.264C112.032,430.944,189.152,432,256.064,432c66.784,0,143.872-1.056,174.56-2.816c30.688-2.08,45.76-6.56,59.648-31.264C504.704,373.504,512,330.688,512,256.192c0,0,0-0.096,0-0.16c0,0,0-0.064,0-0.096C512,181.088,504.704,138.592,490.24,113.92z M192,352V160l160,96L192,352z">
+                                    </path>
+                                </svg>
+                            </a>
                         </div>
                     </div>
+                    <h4 class="title-menu title-menu2">
+                        Hình thức thanh toán
+                    </h4>
+                    <ul class="thanhtoan list-menu hidden-mobile">
+                        <li><img width="108" height="66" alt="Payment 1"
+                                data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_1.png?1735871727390"
+                                class="lazyload error"
+                                src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_1.png?1735871727390"
+                                data-was-processed="true"></li>
+                        <li><img width="108" height="66" alt="Payment 2"
+                                data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_2.png?1735871727390"
+                                class="lazyload error"
+                                src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_2.png?1735871727390"
+                                data-was-processed="true"></li>
+                        <li><img width="108" height="66" alt="Payment 4"
+                                data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_4.png?1735871727390"
+                                class="lazyload error"
+                                src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/payment_4.png?1735871727390"
+                                data-was-processed="true"></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="col-md-12">
-            <center>
-                <span
-                    style="text-shadow: 1px 1px 2px #ffd800,
-                            0 0 25px #000000, 0 0 5px #0000b3;">
-                    Created By
-                    <a href="mailto:dntrung044@gmail.com">Trung</a>
-                    | © No Copyright
-                </span>
-            </center>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12 p-1">
+                    <span class="copy-right">ⓝ<b> ẨM THỰC XỨ QUẢNG</b></span>
+                    <span class="opacity1"> <span class="dash hidden-xs">|</span> Created by
+                        <a href="https://github.com/dntrung044"
+                            rel="noopener" title="Sapo" target="_blank">@dntrung044</a>
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -40,3 +40,57 @@
         </div>
     </div>
 </header>
+{{-- header mobile --}}
+<div class="fixed-bottom-mobile d-lg-none d-block">
+    <ul>
+        <li>
+            <a href="/">
+                <img width="32" height="32" class="lazyload loaded"
+                    src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_6.png?1735871727390"
+                    alt="Trang chủ" data-was-processed="true">
+                <span>Trang chủ</span>
+            </a>
+        </li>
+        <li>
+            <a href="/thuc-don" class="wish-header">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <line y1="4.5" x2="24" y2="4.5" stroke="#000"></line>
+                    <line y1="11.5" x2="24" y2="11.5" stroke="#000"></line>
+                    <line y1="19.5" x2="24" y2="19.5" stroke="#000"></line>
+                </svg>
+                <span class="count_store">8</span>
+                <span>Thực đơn</span>
+            </a>
+        </li>
+        <li>
+            <a href="/blog">
+                <img width="32" height="32" class="lazyload loaded"
+                    src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_5.png?1735871727390"
+                    data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_5.png?1735871727390"
+                    alt="Tin tuc" data-was-processed="true">
+                <span>Tin tức</span>
+            </a>
+        </li>
+        
+        <li>
+            <a href="/gioi-thieu">
+                <img width="32" height="32" class="lazyload loaded"
+                    src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_1.png?1735871727390"
+                    data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_1.png?1735871727390"
+                    alt="Yêu thích" data-was-processed="true">
+                <span>Giới thiệu</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="/dat-ban">
+                <img width="32" height="32" class="lazyload loaded"
+                    src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_1.png?1735871727390"
+                    data-src="//bizweb.dktcdn.net/100/514/629/themes/951567/assets/icon_poly_hea_1.png?1735871727390"
+                    alt="Yêu thích" data-was-processed="true">
+                <span>Đặt bàn</span>
+            </a>
+        </li>
+    </ul>
+</div>
