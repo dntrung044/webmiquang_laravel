99687a2df677efb7b3b7637546d5b14c7bc7058b
Đào Nhật Trung <79350606+caubuonviaiV@users.noreply.github.com>
2025-02-11 10:04:23 +0700

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Aboutus;
 use App\Models\Product;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         view()->composer('*', function($view){
             $carts = Session::get('carts');
             if (is_null($carts)) return [];
--- resources/views/user/layout/head.blade.php
+++ resources/views/user/layout/head.blade.php
@@ -3,31 +3,31 @@
 <title>{{ $title ?? 'Mì Quảng' }}</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- Favicons-->
-<link rel="shortcut icon" href="{{ url('teamplate/img/favicon/favicon.png') }}" type="image/x-icon">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ url('teamplate/img/favicon/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ url('teamplate/img/favicon/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ url('teamplate/img/favicon/favicon-16x16.png') }}">
+<link rel="shortcut icon" href="{{ asset('teamplate/img/favicon/favicon.png') }}" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('teamplate/img/favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('teamplate/img/favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('teamplate/img/favicon/favicon-16x16.png') }}">
 <!-- GOOGLE WEB FONT -->
 <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 <!-- Bootstrap CSS -->
-<link href="{{ url('teamplate/css/bootstrap.css') }}" rel="stylesheet">
-<link href="{{ url('teamplate/css/bootstrap.min.css') }}" rel="stylesheet"> 
+<link href="{{ asset('teamplate/css/bootstrap.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/bootstrap.min.css') }}" rel="stylesheet">
 <!-- Bootstrap Icon -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <!-- BASE CSS -->
-<link href="{{ url('teamplate/css/vendors.min.css') }}" rel="stylesheet">
-<link href="{{ url('teamplate/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/vendors.min.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/style.css') }}" rel="stylesheet">
 <!--==================== -Main css- ====================-->
 <!-- Blog CSS -->
-<link href="{{ url('teamplate/css/blog.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/blog.css') }}" rel="stylesheet">
 <!-- Shop CSS -->
-<link href="{{ url('teamplate/css/shop.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/shop.css') }}" rel="stylesheet">
 <!-- Contact CSS -->
-<link href="{{ url('teamplate/css/wizard.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/wizard.css') }}" rel="stylesheet">
 <!-- Checkout CSS -->
-<link href="{{ url('teamplate/css/order-sign_up.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/order-sign_up.css') }}" rel="stylesheet">
 <!-- User bot CSS -->
-<link href="{{ url('teamplate/css/user-bot.css') }}" rel="stylesheet">
+<link href="{{ asset('teamplate/css/user-bot.css') }}" rel="stylesheet">
 {{-- material-design --}}
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
